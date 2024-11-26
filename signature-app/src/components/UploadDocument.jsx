2ef9485b795fd270d742a5d47909fc8fca9dac86
Lucas Araujo <luc4s.r.a10@gmail.com>
2024-11-26 16:01:22 -0300

    pop-up descricao

--- signature-app/src/components/UploadDocument.jsx
+++ signature-app/src/components/UploadDocument.jsx
@@ -136,7 +136,7 @@ const Textarea = styled.textarea`
   width: 100%;
   height: 150px;
   margin-top: 10px;
-  padding: 1px;
+  padding: 0px;
   font-size: 1rem;
   border-radius: 4px;
   border: 1px solid #ccc;
